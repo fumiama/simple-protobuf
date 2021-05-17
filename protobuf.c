234e414be2add42aace07635c7712581d1a9dada
fumiama <s862105088@gmail.com>
2021-05-17 20:30:18 +0800

    规范化代码

--- protobuf.c
+++ protobuf.c
@@ -80,3 +80,30 @@ int set_pb(FILE* fp, uint8_t* items_type, uint64_t struct_len, void* target) {
     }
     return i;
 }
+
+uint8_t first_set(uint64_t n) {
+    uint8_t i = 0;
+    while(!(n & 1)) {
+        n >>= 1;
+        i++;
+    }
+    return i;
+}
+
+void align_struct(uint8_t* items_type, uint64_t items_cnt, uint64_t struct_size) {
+    uint64_t sum = 0;
+    uint8_t min = 255;
+    for(uint64_t i = 0; i < items_cnt; i++) {
+        sum += 1u << items_type[i];
+        if(min > items_type[i]) min = items_type[i];
+    }
+    while(sum < struct_size) {
+        uint8_t new_min = 255;
+        sum = 0;
+        for(uint64_t i = 0; i < items_cnt; i++) {
+            if(items_type[i] == min) items_type[i]++;
+            if(new_min > items_type[i]) new_min = items_type[i];
+            sum += 1u << items_type[i];
+        }
+    }
+}
